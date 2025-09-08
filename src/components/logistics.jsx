eab4242d351a49bbf2899f2431ac28cbc3dc185d
orange! <156160264+orangegameube@users.noreply.github.com>
2025-09-08 20:02:20 +0000

    Finished schedule css for highlight upon cursor hover, working on logistics panels

---
+++ src/components/logistics.jsx
@@ -0,0 +1,28 @@
+
+
+export const Logistics = () => {
+
+    return (
+        <div className='text-center m-10'>
+            <h1 className='text-3xl font-white leading-tight mb-6 font-weight-bold'>Event Details</h1>
+            <div className='flex'>
+                <div className='rounded-sm bg-white p-14 m-10'>
+                    <img />
+                    <h3>When</h3>
+                    <h5>Saturday, Oct. 4, 2025</h5>
+                </div>
+                <div className='rounded-sm bg-white p-14 m-10'>
+                    <img />
+                    <h3>When</h3>
+                    <h5>Saturday, Oct. 4, 2025</h5>
+                </div>
+                <div className='rounded-sm bg-white p-14 m-10'>
+                    <img />
+                    <h3>When</h3>
+                    <h5>Saturday, Oct. 4, 2025</h5>
+                </div>
+            </div>
+        </div>
+    );
+
+};
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,14 +2,20 @@ import { Front } from '../components/front.jsx';
 import { FAQ } from '../components/faq.jsx';
 import { Nav } from '../components/nav.jsx';
 import { Schedule } from '../components/schedule.jsx';
+import { Logistics } from '../components/logistics.jsx'
 
 export const Home = () => {
     return (
         <div>
             <Nav />
-            <Front />
-            <Schedule />
-            <FAQ />
+            <div>
+                <Front />
+                <div className="bg-slate-950 text-slate-100 overflow-x-hidden">
+                    <Logistics />
+                    <Schedule />
+                    <FAQ />
+                </div>
+            </div>
         </div>
     );
 };
